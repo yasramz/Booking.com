36be461b94789bf882263697d1bab6b2322b7b8f
yasramz <116145800+yasramz@users.noreply.github.com>
2022-12-19 16:31:34 +0330

    CHANGE some fields

--- Quarter/models.py
+++ Quarter/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    address = models.TextField()
+    map_link = models.TextField()
+
+
+class AbstractQuarter(models.Model):
+    DOCTOR_CARE = 1
+    FREE_WIFI = 2
+    MEDICAL_SOCIAL_SERVICE = 3
+    FACILITY_CHOICES = (
+        (DOCTOR_CARE, 'Doctor_Care'),
+        (FREE_WIFI, 'Free_WIFI'),
+        (MEDICAL_SOCIAL_SERVICE, 'Media_Social_Service')
+    )
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    capacity = models.IntegerField()
+    price = models.IntegerField()
+    check_Out = models.DateTimeField()
+    check_In = models.DateTimeField()
+    facility = models.PositiveSmallIntegerField(choices=FACILITY_CHOICES, default=FREE_WIFI)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    class Mete:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class Hotel(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    # available = models.IntegerField()
+    avatar = None
+
+
+class General_Avatar(models.Model):
+    avatar = models.ImageField(upload_to='Quarter/hotel/avatar')
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+
+
+class Detail_Avatar(models.Model):
+    avatar = models.ImageField(upload_to='Quarter/quarter/avatar')
+    quarter = models.ForeignKey(AbstractQuarter, on_delete=models.CASCADE)
+
+
+class Villa(AbstractQuarter):
+    AVAILABLE = 1
+    FULL = 2
+    AVAILABLENESS_CHOICES = (
+        (AVAILABLE, 'Available'),
+        (FULL, 'Full')
+    )
+
+    Available = models.PositiveSmallIntegerField(choices=AVAILABLENESS_CHOICES, default=AVAILABLE)
+
+
+class HotelRoom(AbstractQuarter):
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    # availableness = None
