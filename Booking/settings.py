6f83318c01dbfa990ba77ce3be5e33057d49b704
yasramz <116145800+yasramz@users.noreply.github.com>
2022-12-20 13:18:18 +0330

    SET AUTH_USER_MODEL in setting.py

--- Booking/settings.py
+++ Booking/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 from decouple import config, Csv
 
@@ -39,9 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'Quarter.apps.QuarterConfig',
-    'User.apps.UserConfig',
-    'Vehicle.apps.VehicleConfig',
+    'quarter.apps.QuarterConfig',
+    'user.apps.UserConfig',
+    'vehicle.apps.VehicleConfig',
 
     'rest_framework'
 ]
@@ -124,7 +124,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = 'media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.user'
+
--- quarter/apps.py
+++ quarter/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class QuarterConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Quarter'
+    name = 'quarter'
