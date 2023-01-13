bafe7a8395f709f702eaaa639a05be32560bf76f
yasramz <116145800+yasramz@users.noreply.github.com>
2023-01-13 14:00:00 +0330

    Add HotelCommentViewSet, HotelRoomCommentViewSet, VillaCommentViewSet, HotelRateViewSet, HotelRoomRateViewSet, VillaRateViewSet and Set url for each viewset

--- quarter/api/urls.py
+++ quarter/api/urls.py
@@ -1,11 +1,17 @@
 from rest_framework.routers import DefaultRouter
-from quarter.api.views import VillaViewSet, HotelViewSet, HotelRoomViewSet
+from quarter.api.views import *
 
 router = DefaultRouter()
 
 router.register(r'villa', VillaViewSet, basename='villa')
 router.register(r'hotel', HotelViewSet, basename='hotel')
 router.register(r'hotelroom', HotelRoomViewSet, basename='hotelroom')
+router.register(r'hotel/comment', HotelCommentViewSet, basename='hotel/comment')
+router.register(r'hotelroom/comment', HotelRoomCommentViewSet, basename='hotelroom/comment')
+router.register(r'villa/comment', VillaCommentViewSet, basename='villa/comment')
+router.register(r'hotel/rate', HotelRateViewSet, basename='hotel/rate')
+router.register(r'hotelroom/rate', HotelRoomRateViewSet, basename='hotelroom/rate')
+router.register(r'villa/room', VillaRateViewSet, basename='villa/room')
 
 
 urlpatterns = [
--- quarter/api/views.py
+++ quarter/api/views.py
@@ -1,13 +1,15 @@
-from quarter.api.serializer import VillaSerializer, HotelSerializer, HotelRoomSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from quarter.api.serializer import *
 from rest_framework import mixins, viewsets
-from quarter.models import Villa, Hotel, HotelRoom
+from quarter.models import *
 from filtering.filter import VillaFilterSet, HotelRoomFilterSet
 
 
 class VillaViewSet(mixins.RetrieveModelMixin,
                    mixins.ListModelMixin,
                    viewsets.GenericViewSet):
-
     queryset = Villa.objects.all()
     serializer_class = VillaSerializer
     filterset_class = VillaFilterSet
@@ -16,7 +18,6 @@ class VillaViewSet(mixins.RetrieveModelMixin,
 class HotelViewSet(mixins.RetrieveModelMixin,
                    mixins.ListModelMixin,
                    viewsets.GenericViewSet):
-
     queryset = Hotel.objects.all()
     serializer_class = HotelSerializer
 
@@ -24,7 +25,60 @@ class HotelViewSet(mixins.RetrieveModelMixin,
 class HotelRoomViewSet(mixins.RetrieveModelMixin,
                        mixins.ListModelMixin,
                        viewsets.GenericViewSet):
-
     queryset = HotelRoom.objects.all()
     serializer_class = HotelRoomSerializer
     filterset_class = HotelRoomFilterSet
+
+
+class HotelCommentViewSet(mixins.CreateModelMixin,
+                          mixins.UpdateModelMixin,
+                          viewsets.GenericViewSet):
+    serializer_class = HotelCommentSerializer
+    queryset = HotelComment.objects.all()
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = [IsAuthenticated]
+
+
+class HotelRoomCommentViewSet(mixins.CreateModelMixin,
+                              mixins.UpdateModelMixin,
+                              viewsets.GenericViewSet):
+    serializer_class = HotelRoomCommentSerializer
+    queryset = HotelRoomComment.objects.all()
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = [IsAuthenticated]
+
+
+class VillaCommentViewSet(mixins.CreateModelMixin,
+                          mixins.UpdateModelMixin,
+                          viewsets.GenericViewSet):
+    serializer_class = VillaCommentSerializer
+    queryset = VillaComment.objects.all()
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = [IsAuthenticated]
+
+
+class HotelRateViewSet(mixins.CreateModelMixin,
+                       mixins.UpdateModelMixin,
+                       viewsets.GenericViewSet):
+    serializer_class = HotelRateSerializer
+    queryset = HotelRate.objects.all()
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = [IsAuthenticated]
+
+
+class HotelRoomRateViewSet(mixins.CreateModelMixin,
+                           mixins.UpdateModelMixin,
+                           viewsets.GenericViewSet):
+    serializer_class = HotelRoomSerializer
+    queryset = HotelRoomRate.objects.all()
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = [IsAuthenticated]
+
+
+class VillaRateViewSet(mixins.CreateModelMixin,
+                       mixins.UpdateModelMixin,
+                       viewsets.GenericViewSet):
+    serializer_class = VillaRateSerializer
+    queryset = HotelComment.objects.all()
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = [IsAuthenticated]
