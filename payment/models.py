6bf63444daeb130c8c72ea10991a9c859fcc09ea
Yasramz <yasramezani@gmail.com>
2023-01-14 14:19:09 +0330

    CleanCode for payment.models

--- payment/models.py
+++ payment/models.py
@@ -3,9 +3,11 @@ from book.models import HotelRoomReservation, VillaReservation
 from user.models import User
 
 
+# -------------------------------------------Payment History Models-----------------------------------------------------
 class HotelRoomPaymentHistory(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_payment')
-    reservation_obj = models.ForeignKey(HotelRoomReservation, on_delete=models.DO_NOTHING, related_name='hotelroom_payment_history')
+    reservation_obj = models.ForeignKey(HotelRoomReservation, on_delete=models.DO_NOTHING,
+                                        related_name='hotelroom_payment_history')
     is_valid = models.BooleanField(default=True)
     date = models.DateTimeField(auto_now_add=True)
     modified_time = models.DateTimeField(auto_now=True)
@@ -13,8 +15,9 @@ class HotelRoomPaymentHistory(models.Model):
 
 class VillaPaymentHistory(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_villa_payment')
-    reservation_obj = models.ForeignKey(VillaReservation, on_delete=models.DO_NOTHING, related_name='villa_payment_history')
+    reservation_obj = models.ForeignKey(VillaReservation, on_delete=models.DO_NOTHING,
+                                        related_name='villa_payment_history')
     is_valid = models.BooleanField(default=True)
     date = models.DateTimeField(auto_now_add=True)
     modified_time = models.DateTimeField(auto_now=True)
-
+# ----------------------------------------------------------------------------------------------------------------------
