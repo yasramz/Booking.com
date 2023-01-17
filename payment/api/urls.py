c52d69506f493c4653d6b2dbfc0c7309dcdc47d3
yasramz <yasramezani@gmail.com>
2023-01-17 23:58:27 +0330

    CleanCode for payment.api.urls

--- payment/api/urls.py
+++ payment/api/urls.py
@@ -4,6 +4,7 @@ from payment.api.views import HotelRoomPaymentHistoryViewSet, VillaPaymentHistor
 
 router = DefaultRouter()
 
+# Quarter Payment History Router Urls:
 router.register(r'hotel-room-payment', HotelRoomPaymentHistoryViewSet, basename='hotel_room_payment')
 router.register(r'villa-payment', VillaPaymentHistoryViewSet, basename='villa_payment')
 
