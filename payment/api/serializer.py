580b9698af89a9ae47c5fc443870616edc817b90
yasramz <yasramezani@gmail.com>
2023-01-17 23:57:19 +0330

    CleanCode for payment.api.serializers

--- payment/api/serializer.py
+++ payment/api/serializer.py
@@ -2,15 +2,20 @@ from rest_framework import serializers
 from payment.models import *
 
 
+# ---------------------------------------- Payment History Serializers -------------------------------------------------
+
+# Hotel Room:
 class HotelRoomPaymentHistorySerializer(serializers.ModelSerializer):
     class Meta:
         model = HotelRoomPaymentHistory
         fields = ('id', 'user', 'reservation_obj', 'date', 'is_valid', )
 
 
+# Villa:
 class VillaPaymentHistorySerializer(serializers.ModelSerializer):
     class Meta:
         model = VillaPaymentHistory
         fields = ('id', 'user', 'reservation_obj', 'date', 'is_valid',)
 
 
+# ----------------------------------------------------------------------------------------------------------------------
