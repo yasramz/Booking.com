b4643f3b7c224f041c1328d8b6663656d58912f8
yasramz <yasramezani@gmail.com>
2023-01-18 00:06:35 +0330

    CleanCode for quarter.api.urls

--- quarter/api/urls.py
+++ quarter/api/urls.py
@@ -3,15 +3,20 @@ from quarter.api.views import *
 
 router = DefaultRouter()
 
-router.register(r'villa', VillaViewSet, basename='villa')
+# Hotel Router Urls:
 router.register(r'hotel', HotelViewSet, basename='hotel')
-router.register(r'hotelroom', HotelRoomViewSet, basename='hotelroom')
 router.register(r'hotel/comment', HotelCommentViewSet, basename='hotel/comment')
-router.register(r'hotelroom/comment', HotelRoomCommentViewSet, basename='hotelroom/comment')
-router.register(r'villa/comment', VillaCommentViewSet, basename='villa/comment')
 router.register(r'hotel/rate', HotelRateViewSet, basename='hotel/rate')
+
+# Hotel Room Router Urls:
+router.register(r'hotelroom', HotelRoomViewSet, basename='hotelroom')
+router.register(r'hotelroom/comment', HotelRoomCommentViewSet, basename='hotelroom/comment')
 router.register(r'hotelroom/rate', HotelRoomRateViewSet, basename='hotelroom/rate')
-router.register(r'villa/room', VillaRateViewSet, basename='villa/room')
+
+# Villa Router urls:
+router.register(r'villa', VillaViewSet, basename='villa')
+router.register(r'villa/comment', VillaCommentViewSet, basename='villa/comment')
+router.register(r'villa/rate', VillaRateViewSet, basename='villa/rate')
 
 
 urlpatterns = [
